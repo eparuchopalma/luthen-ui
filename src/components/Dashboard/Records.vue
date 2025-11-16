e00b43b9c9442e8d7140bb95b619f2e378bede6e
Edgar Parucho Palma <eparuchopalma@outlook.com>
2025-11-16 15:46:53 -0400

    Add feature: table controls (pages navigation). - Add styles and bind buttons according to logic (disabled, first / last page). - Add logic to alternate record rows on pages navigation.

--- src/components/Dashboard/Records.vue
+++ src/components/Dashboard/Records.vue
@@ -15,15 +15,34 @@ async function setRecords() {
 
 const screenHeight = ref(window.innerHeight)
 
-const rowsQuantity = ref(10)
+const totalPages = ref(1)
+const currentPage = ref(1)
+const rowsPerPage = ref(10)
 
 const pageRecords = computed(() => {
-  return recordStore.records.slice(0, rowsQuantity.value)
+  const startingIndex = (currentPage.value - 1) * rowsPerPage.value
+  const endingIndex = startingIndex + rowsPerPage.value
+  return recordStore.records.slice(startingIndex, endingIndex)
 })
 
-const handleResize = () => screenHeight.value = window.innerHeight
+function setNextPage() {
+  currentPage.value++
+}
 
-watch(screenHeight, (newHeight) => rowsQuantity.value = Math.floor(newHeight / 44))
+function setPreviousPage() {
+  currentPage.value--
+}
+
+function handleResize() {
+  screenHeight.value = window.innerHeight
+}
+
+watch([screenHeight, () => recordStore.records], ([newHeight, newRecords]) => {
+  const rowsToFit = Math.floor(newHeight / 44)
+  rowsPerPage.value = rowsToFit
+  totalPages.value = Math.ceil(newRecords.length / rowsToFit)
+}, { immediate: true, deep: true }
+)
 
 window.addEventListener('resize', handleResize)
 
@@ -47,48 +66,59 @@ window.addEventListener('resize', handleResize)
       type="button"
       >Export</button>
     </div>
-    <table class="table">
-      <caption class="table__caption">Click on a record for details and actions.</caption>
-      <thead>
-        <tr class="table__header-row">
-          <th>Date</th>
-          <th>Tag</th>
-          <th>Amount</th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr v-for="record in pageRecords" :key="record.id" class="table__body-row">
-          <td class="table__cell table__cell_text-left">{{ record.date.slice(0, 10) }}</td>
-          <td class="table__cell table__cell_text-left">{{ record.tag }}</td>
-          <td class="table__cell table__cell_text-right">$ {{ record.amount }}</td>
-        </tr>
-      </tbody>
-      <tfoot>
-        <tr class="table__footer-row">
-          <td>
-            <div class="table__footer-data">
-              <span>Credit</span>
-              <span>$ 0</span>
-            </div>
-          </td>
-          <td>
-            <div class="table__footer-data">
-              <span>Debit</span>
-              <span>$ -15</span>
-            </div>
-          </td>
-          <td>
-            <div class="table__footer-data">
-              <span>Total</span>
-              <span>$ -15</span>
-            </div>
-          </td>
-        </tr>
-      </tfoot>
-    </table>
-    <div class="table-actions">
-      <button class="button button_sm button_dark" type="button">&lt</button>
-      <button class="button button_sm button_dark" type="button">&gt;</button>
+    <div class="table-container">
+      <table class="table">
+        <caption class="table__caption">Click on a record for details and actions.</caption>
+        <thead>
+          <tr class="table__header-row">
+            <th>Date</th>
+            <th>Tag</th>
+            <th>Amount</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr v-for="record in pageRecords" :key="record.id" class="table__body-row">
+            <td class="table__cell table__cell_text-left">{{ record.date.slice(0, 10) }}</td>
+            <td class="table__cell table__cell_text-left">{{ record.tag }}</td>
+            <td class="table__cell table__cell_text-right">$ {{ record.amount }}</td>
+          </tr>
+        </tbody>
+        <tfoot>
+          <tr class="table__footer-row">
+            <td>
+              <div class="table__footer-data">
+                <span>Credit</span>
+                <span>$ 0</span>
+              </div>
+            </td>
+            <td>
+              <div class="table__footer-data">
+                <span>Debit</span>
+                <span>$ -15</span>
+              </div>
+            </td>
+            <td>
+              <div class="table__footer-data">
+                <span>Total</span>
+                <span>$ -15</span>
+              </div>
+            </td>
+          </tr>
+        </tfoot>
+      </table>
+      <div class="table-actions">
+        <button
+        class="button button_sm button_dark"
+        type="button"
+        @click="setPreviousPage"
+        :disabled="currentPage == 1">&lt</button>
+        <small>Page {{ currentPage }} / {{ totalPages }}. Records: {{ recordStore.records.length }}</small>
+        <button
+        class="button button_sm button_dark"
+        type="button"
+        @click="setNextPage"
+        :disabled="currentPage == totalPages">&gt;</button>
+      </div>
     </div>
   </section>
 </template>
@@ -96,15 +126,23 @@ window.addEventListener('resize', handleResize)
 <style scoped>
 
 .table {
-  margin: 48px auto 32px;
   width: 100%;
-  max-width: 500px;
+  margin: 0 auto;
   border-spacing: 2px;
   border-collapse: separate;
   font-size: 1.4rem;
   table-layout: fixed;
 }
 
+.table-container {
+  margin: 48px auto 32px;
+  width: 100%;
+  max-width: 500px;
+  height: 64vh;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+}
   
 .table__caption {
   margin: 8px 0;
@@ -144,8 +182,7 @@ window.addEventListener('resize', handleResize)
 }
 
 .table-actions {
-  max-width: 500px;
-  margin: 0 auto;
+  width: 100%;
   display: flex;
   justify-content: space-between;
 }
