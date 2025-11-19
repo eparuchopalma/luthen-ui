c4e00029f4aede1c4f7728c7a982aa166b9842e6
Edgar Parucho Palma <eparuchopalma@outlook.com>
2025-11-18 21:37:20 -0400

    Set up the logic to display real info in tags table of Insights section.

--- src/components/Dashboard/Insights.vue
+++ src/components/Dashboard/Insights.vue
@@ -1,4 +1,27 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { ref, watch } from 'vue';
+import { recordStore } from "../../store/recordStore"
+
+const tagsData = ref()
+const listType = ref<1 | 2>(2)
+const tagListBalance = ref(0)
+
+function setTagsData() {
+  const typeRecords = recordStore.records.filter(r => r.type === listType.value)
+  tagListBalance.value = 0
+  tagsData.value = Object.entries(typeRecords.reduce((acc: any, current) => {
+    if (acc[current.tag]) acc[current.tag] += Number(current.amount)
+    else acc[current.tag] = Number(current.amount)
+    tagListBalance.value += Number(current.amount)
+    return acc
+  }, {}))
+}
+
+watch([listType, () => recordStore.records], (newListType, newRecords) => {
+  setTagsData()
+}, { immediate: true })
+
+</script>
 
 <template>
   <section class="section">
@@ -8,36 +31,44 @@
       </div>
       <h1>Insights</h1>
     </header>
-    <!-- <div class="table-container"> -->
-      <table class="table">
-        <thead>
-          <tr class="table__header-row">
-            <th>Tag</th>
-            <th>Balance</th>
-            <th>Equivalence</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr>
-            <td class="table__cell table__cell_text-left">Home</td>
-            <td class="table__cell table__cell_text-right">- $700.00</td>
-            <td class="table__cell">70%</td>
-          </tr>
-          <tr>
-            <td class="table__cell table__cell_text-left">Vehicle</td>
-            <td class="table__cell table__cell_text-right">- $300.00</td>
-            <td class="table__cell">30%</td>
-          </tr>
-        </tbody>
-      </table>
-    <!-- </div> -->
+    <select id="tag-list-type" class="select-field" v-model="listType">
+      <option value="2" class="option">Debit</option>
+      <option value="1" class="option">Credit</option>
+    </select>
+    <table class="table">
+      <thead>
+        <tr class="table__header-row">
+          <th>Tag</th>
+          <th>Balance</th>
+          <th>Equivalence</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="[tagName, tagBalance] in tagsData" :key="tagName">
+          <td class="table__cell table__cell_text-left">{{ tagName }}</td>
+          <td class="table__cell table__cell_text-right">- ${{ tagBalance }}</td>
+          <td class="table__cell">{{ Math.round((tagBalance / tagListBalance) * 100) }}%</td>
+        </tr>
+      </tbody>
+    </table>
   </section>
 </template>
 
 <style scoped>
 
+.select-field {
+  margin-top: 48px;
+  border-radius: 2px;
+  width: 80px;
+}
+
+.option {
+  background-color: var(--darkest);
+  color: var(--lightest);
+}
+
 .table {
-  margin: 48px auto 0;
+  margin: 12px auto 0;
   width: 100%;
   max-width: 400px;
   border-spacing: 2px;
