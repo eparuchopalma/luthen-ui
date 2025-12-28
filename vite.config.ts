121ff7992240171d6daed64beb2c609733200d3d
Edgar Parucho Palma <eparuchopalma@outlook.com>
2025-12-28 12:23:52 -0400

    Add Progressive Web App feature.

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,25 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      manifest: {
+        name: 'Luthen: Financial Log.',
+        short_name: 'Luthen',
+        start_url: '/',
+        display: 'fullscreen',
+        background_color: '#242225',
+        theme_color: '#BC5536',
+        icons: [
+          { src: 'icons/pwa-192x192.png', sizes: '192x192', type: 'image/png' },
+          { src: 'icons/pwa-512x512.png', sizes: '512x512', type: 'image/png' }
+        ]
+      }
+    })
+  ],
 })
